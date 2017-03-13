c233f1d4a4f45fcba40846d74c520666f5f5a232
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-13 16:20:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pyanyzip
+pyanyzip is a module to help with dealing with all compressions transparently.
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+import sys
+if not sys.version_info[0] == 3:
+    sys.exit("Sorry, only python version 3 is supported")
+
+setuptools.setup(
+    name='pyanyzip',
+    version='0.0.1',
+    description='pyanyzip is a module to help with dealing with compressed files transparently',
+    long_description='pyanyzip is a module to help with dealing with compressed files transparently',
+    url='https://veltzer.github.io/pyanyzip',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='python zip bz2 gzip',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+)
