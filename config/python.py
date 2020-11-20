5e317de8b32f7aa2642bf129b6e842ea38fa47cc
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-20 12:37:14 +0200

    dependencies do not need explanations

--- config/python.py
+++ config/python.py
@@ -9,25 +9,25 @@ setup_requires = [
 ]
 
 run_requires = [
-    'pypipegzip',  # for opening zipped files
-    'pylzma',  # for working with .xz files
-    'typing',  # for types
+    'pypipegzip',
+    'pylzma',
+    'typing',
 ]
 
 test_requires = [
-    'pylint',  # to check for lint errors
-    'pytest',  # for testing
-    'pytest-cov',  # for testing
-    'pyflakes',  # for testing
-    'flake8',  # for linting
-    'pymakehelper',  # for the makefile
+    'pylint',
+    'pytest',
+    'pytest-cov',
+    'pyflakes',
+    'flake8',
+    'pymakehelper',
 ]
 
 dev_requires = [
-    'pyclassifiers',  # for programmatic classifiers
-    'pypitools',  # for upload etc
-    'pydmt',  # for building
-    'Sphinx',  # for the sphinx builder
+    'pyclassifiers',
+    'pypitools',
+    'pydmt',
+    'Sphinx',
 ]
 
 install_requires = list(setup_requires)
@@ -36,5 +36,4 @@ install_requires.extend(run_requires)
 python_requires = ">=3.6"
 
 extras_require = {
-    # ':python_version == "2.7"': ['futures'],  # for python2.7 backport of concurrent.futures
 }
