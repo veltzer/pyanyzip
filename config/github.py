d92f010abbbc62bc3f0568328a99bb66c4296200
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 00:57:55 +0200

    no commit message given

--- config/github.py
+++ config/github.py
@@ -2,6 +2,7 @@ import pydmt.helpers.project
 
 if pydmt.helpers.project.get_name().startswith("py"):
     workflows_platforms = [
+        ("ubuntu-22.04", "3.12"),
         ("ubuntu-22.04", "3.11"),
         ("ubuntu-22.04", "3.10"),
         ("ubuntu-20.04", "3.9"),
