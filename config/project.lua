2ade9f79d5a09cd616c401f1c4c09b2db35eae8d
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:21 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,8 @@
+NAME = "pyanyzip"
+DESCRIPTION_SHORT = "pyanyzip is a module to help with dealing with compressed files transparently"
+KEYWORDS = {
+    "python",
+    "zip",
+    "bz2",
+    "gzip",
+}
--- config/project.py
+++
@@ -1,9 +0,0 @@
-""" project definitions """
-name = "pyanyzip"
-description_short = "pyanyzip is a module to help with dealing with compressed files transparently"
-keywords = [
-    "python",
-    "zip",
-    "bz2",
-    "gzip",
-]
